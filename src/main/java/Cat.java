e7a936b605505d7f8016cd4fac4ae8d41e872b72
Larisa <lara_gil@mail.ru>
2022-11-24 23:56:38 +0500

    commit 1 for task 2.1.3

---
+++ src/main/java/Cat.java
@@ -0,0 +1,43 @@
+import java.util.Objects;
+
+public class Cat {
+    public String name = "Vasja";
+
+    public Cat() {
+
+    }
+
+    public Cat(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cat cat = (Cat) o;
+        return Objects.equals(name, cat.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
+    public String sayName() {
+        return "I'm " + name + " cat. Mew.";
+    }
+}
